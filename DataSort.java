3076dade54fbace9183d4738ca5fe2f439b64f7a
Javier <jrenteria3@csustan.edu>
2018-03-21 13:59:32 -0700

    Newest

--- DataSort.java
+++ DataSort.java
@@ -27,9 +27,7 @@ public class DataSort {
 	public static int ACC_count = 0;
 	public static int YEAR_count = 0;
 	public static int ORI_count = 0;
-	
 	public static int CAR_count =  0;
-	public static int modelCC = 0;
 	
 	public static LinkedList<Integer> MPG_ll = new LinkedList<Integer>();
 	public static LinkedList<Integer> CYL_ll = new LinkedList<Integer>();
@@ -40,24 +38,45 @@ public class DataSort {
 	public static LinkedList<Integer> YEAR_ll = new LinkedList<Integer>();
 	public static LinkedList<Integer> ORI_ll = new LinkedList<Integer>();
 	
+	public static int[] mpgArr = new int[20];
+	public static int mpg;
+	
+	public static ArrayList<Integer> MPG_arr = new ArrayList<Integer>();
+	public static ArrayList<Integer> CYL_arr = new ArrayList<Integer>();
+	public static ArrayList<Integer> DIS_arr = new ArrayList<Integer>();
+	public static ArrayList<Integer> HP_arr = new ArrayList<Integer>();
+	public static ArrayList<Integer> WEI_arr = new ArrayList<Integer>();
+	public static ArrayList<Integer> ACC_arr = new ArrayList<Integer>();
+	public static ArrayList<Integer> YEAR_arr = new ArrayList<Integer>();
+	public static ArrayList<Integer> ORI_arr = new ArrayList<Integer>();
+	
+	public static LinkedList<String> BillysList = new LinkedList<String>();
+	
+	public static LinkedList<String> MakeModel = new LinkedList<String>();
 	public static LinkedList<String> CAR = new LinkedList<String>();
-	public static LinkedList<String> MODEL = new LinkedList<String>();
 	
 	public static void main(String[] args) {
 		
-		DataSort s = new DataSort();
+		DataSort data = new DataSort();
 		
-		s.openMakeModel();
-		s.readMakeModel();
-		s.closeFile();
+		data.openFile();
+		data.readFile();
+		data.closeFile();
 		
-		System.out.println(CAR);
+		data.sortContents();
+		data.printContents();
+
+		data.openAllData();
+		data.readAllData();
+		data.closeFile();
 		
-				
-		s.findMakes("chevrolet-impala");
-		
-		
+	
 	}
+	
+	/*
+	 * 	Printing Contents 
+	 * 
+	 */
 	
 	void printTest(){
 		
@@ -83,51 +102,97 @@ public class DataSort {
 		
 	}
 	
-	void printMAkeModel() {
+	void printBilly() {
 		
-		for(String make : CAR) {
-			//System.out.println("-----------------");
-			System.out.println("|" + make + "|");
+		for(String car: BillysList) {
+			
+			System.out.println("___________");
+			System.out.println(x);
 			
 		}
-			
+		
 		
 		
 	}
 	
-	//-----------------------------------------------------------------//
-	
-	public void binarySearchForValue(int value){
+	void printMakeModel() {
 		
-		int lowIndex = 0;
-		int highIndex = MPG_ll.size() - 1;
-		
-		while(lowIndex <= highIndex){
-			
-			int middleIndex = (highIndex + lowIndex) / 2;
-			
-			if(MPG_ll.get(middleIndex) < value) lowIndex = middleIndex + 1;
-			
-			else if (MPG_ll.get(middleIndex) > value) highIndex = middleIndex - 1;
-			
-			else{
-				
-				System.out.println("\nFound a match for " + value + " at index " + middleIndex);
-				
-				lowIndex = highIndex +1;	
-			}
-				
+		for(String make : MakeModel) {
+			System.out.println("|" + make + "|");
 		}	
 	}
 	
-public void swapValues(int indexOne, int indexTwo){
+	public void printSorted(){
+		//Prints sorted LL
+		System.out.println("\nMPG Linked List SORTED: ");
+		System.out.println(MPG_ll);
 		
-		int temp = MPG_ll.get(indexOne);
-		MPG_ll.get(indexTwo);
-		MPG_ll.set(indexOne, indexTwo);
-		MPG_ll.set(indexTwo, temp);	
+		System.out.println("\nCYLINDERS Linked List SORTED: ");
+		System.out.println(CYL_ll);
+		
+		System.out.println("\nDISPLACEMENT Linked List SORTED: ");
+		System.out.println(DIS_ll);
+		
+		System.out.println("\nHorsePower Linked List SORTED: ");
+		System.out.println(HP_ll);
+		
+		System.out.println("\nWEIGHT Linked List SORTED: ");
+		System.out.println(WEI_ll);
+		
+		System.out.println("\nACCELERATION Linked List SORTED: ");
+		System.out.println(ACC_ll);
+		
+		System.out.println("\nYEAR Linked List SORTED: ");
+		System.out.println(YEAR_ll);
+		
+		System.out.println("\nORIGIN Linked List SORTED: ");
+		System.out.println(ORI_ll);
+		
 	}
+	
+	public void printContents(){
+		//PRINTS UNSORTED LL
+		System.out.println("Number of variables: " + count);	
+			
+		System.out.println("\nMPG List: ");
+		System.out.println(MPG_ll);
+		
+		System.out.println("\nCYLINDERS List: ");
+		System.out.println(CYL_ll);
+		
+		System.out.println("\nDISPLACEMENT List: ");
+		System.out.println(DIS_ll);
+		
+		System.out.println("\nHorsePower List: ");
+		System.out.println(HP_ll);
+		
+		System.out.println("\nWEIGHT List: ");
+		System.out.println(WEI_ll);
+		
+		System.out.println("\nACCELERATION List: ");
+		System.out.println(ACC_ll);
+		
+		System.out.println("\nYEAR List: ");
+		System.out.println(YEAR_ll);
+		
+		System.out.println("\nORIGIN List: ");
+		System.out.println(ORI_ll);
+		
+		}
 
+	public void printAll() {
+		
+		for(String all : CAR) {
+			System.out.println("|" + all + "|");
+		}
+		
+	}
+	
+	/*
+	 * 	Searching Methods 
+	 * 
+	 */
+	
 	public void findMakes(String x) {
 		
 		int numMake = 0;
@@ -223,6 +288,74 @@ public void swapValues(int indexOne, int indexTwo){
 		
 	}
 	
+	public void findCars(String a,String b, String c, String d) {
+		
+		int carcc = 0;
+		
+		for(String car: CAR) {
+			
+			if( car.contains(a) || car.contains(b) || car.contains(c) || car.contains(d) ) {
+			carcc++;	
+			System.out.println(car);
+			System.out.println(carcc);
+			BillysList.add(car);
+			}
+		}
+		
+	}
+	
+	public void findMPG(String a, String b) {
+		
+		for(String car: CAR) {
+			
+			if( car.contains(a)) {
+			
+				
+			}
+		}
+		
+		
+	}
+	
+	public void binarySearchForValue(int value){
+		
+		int lowIndex = 0;
+		int highIndex = MPG_ll.size() - 1;
+		
+		while(lowIndex <= highIndex){
+			
+			int middleIndex = (highIndex + lowIndex) / 2;
+			
+			if(MPG_ll.get(middleIndex) < value) lowIndex = middleIndex + 1;
+			
+			else if (MPG_ll.get(middleIndex) > value) highIndex = middleIndex - 1;
+			
+			else{
+				
+				System.out.println("\nFound a match for " + value + " at index " + middleIndex);
+				
+				lowIndex = highIndex +1;	
+			}
+				
+		}	
+	}
+	
+	
+	public void swapValues(int indexOne, int indexTwo){
+		
+		int temp = MPG_ll.get(indexOne);
+		MPG_ll.get(indexTwo);
+		MPG_ll.set(indexOne, indexTwo);
+		MPG_ll.set(indexTwo, temp);	
+	}
+
+	
+	/*
+	 * 	Insertion and Deletion Methods
+	 * 	Math and other methods 
+	 * 
+	 */
+	
 	public void removeAll(String x, int b){
 		System.out.println("mpg count before: " + MPG_count);
 		if( x.equalsIgnoreCase("mpg") || x.equalsIgnoreCase("1") ){
@@ -261,7 +394,7 @@ public void swapValues(int indexOne, int indexTwo){
 				YEAR_count--;
 			}
 		}else if(x.equalsIgnoreCase("ori") || x.equalsIgnoreCase("origin") || x == "8" ){
-			for(double i = 0; i < ORI_count; i++){
+			for(int i = 0; i < ORI_count; i++){
 				ORI_ll.remove(b);
 				ORI_count--;
 			}
@@ -269,6 +402,7 @@ public void swapValues(int indexOne, int indexTwo){
 	}
 	
 	public void addToList(String x, int b){
+
 		// mpg cyl dis hp wei acc year orig
 		if( x.equalsIgnoreCase("mpg") || x.equalsIgnoreCase("1") ){
 				MPG_ll.add(b);
@@ -297,93 +431,176 @@ public void swapValues(int indexOne, int indexTwo){
 		}
 	}	
 	
-	public void removeFromList(int i, int a){
-		if(i == 1){
-			MPG_ll.remove((int) a);
-		}
-		if(i == 2){
-			CYL_ll.remove((int) a);		
-		}
-		if(i == 3){
-			DIS_ll.remove((int) a);		
+	public void removeFromList(String x, int a){
+			
+			if( x.equalsIgnoreCase("mpg")){
+				System.out.println(MPG_ll.peek());
+				MPG_ll.remove(a);
 			}
-		if(i == 4){
-			HP_ll.remove((int) a);		
+			else if(x.equalsIgnoreCase("cyl") || x.equalsIgnoreCase("cylinders") ){
+				CYL_ll.remove(a);		
 			}
-		if(i == 5){
-			WEI_ll.remove((int) a);		
+			else if(x.equalsIgnoreCase("dis") || x.equalsIgnoreCase("displacement") ){
+				DIS_ll.remove(a);		
 			}
-		if(i == 6){
-			ACC_ll.remove((int) a);		
+			else if(x.equalsIgnoreCase("hp") || x.equalsIgnoreCase("horsepower")){
+			 	HP_ll.remove(a);		
 			}
-		if(i == 7){
-			YEAR_ll.remove((int) a);		
+			else if(x.equalsIgnoreCase("wei") || x.equalsIgnoreCase("weight")){
+			 	WEI_ll.remove(a);		
 			}
-		if(i == 8){
-			ORI_ll.remove((int) a);		
+			else if(x.equalsIgnoreCase("acc") || x.equalsIgnoreCase("acceleration")){
+			 	ACC_ll.remove(a);		
+			}
+			else if(x.equalsIgnoreCase("year") || x.equalsIgnoreCase("yea")){
+			 	YEAR_ll.remove(a);		
+			}
+			else if(x.equalsIgnoreCase("ori") || x.equalsIgnoreCase("origin")){
+			 	ORI_ll.remove(a);		
 			}
 		
 	}
 	
-	
-	public void printContents(){
-	//PRINTS UNSORTED LL
-	System.out.println("Number of variables: " + count);	
+	public void findMost(String x) {
 		
-	System.out.println("\nMPG Linked List: ");
-	System.out.println(MPG_ll);
-	
-	System.out.println("\nCYLINDERS Linked List: ");
-	System.out.println(CYL_ll);
-	
-	System.out.println("\nDISPLACEMENT Linked List: ");
-	System.out.println(DIS_ll);
-	
-	System.out.println("\nHorsePower Linked List: ");
-	System.out.println(HP_ll);
-	
-	System.out.println("\nWEIGHT Linked List: ");
-	System.out.println(WEI_ll);
-	
-	System.out.println("\nACCELERATION Linked List: ");
-	System.out.println(ACC_ll);
-	
-	System.out.println("\nYEAR Linked List: ");
-	System.out.println(YEAR_ll);
-	
-	System.out.println("\nORIGIN Linked List: ");
-	System.out.println(ORI_ll);
-	
+		if( x.equalsIgnoreCase("mpg") || x.equalsIgnoreCase("1") ){
+			System.out.println("Highest MPG: "+ MPG_ll.getLast());
+		
+		} else if(x.equalsIgnoreCase("cyl") || x.equalsIgnoreCase("cylinders") ){
+			System.out.println("Highest CYL: "+CYL_ll.getLast());
+		
+		} else if(x.equalsIgnoreCase("dis") || x.equalsIgnoreCase("displacement") ){
+			System.out.println("Highest DIS: "+DIS_ll.getLast());
+
+		} else if(x.equalsIgnoreCase("hp") || x.equalsIgnoreCase("horsepower")){
+			System.out.println("Highest HP: "+HP_ll.getLast());
+
+		} else if(x.equalsIgnoreCase("wei") || x.equalsIgnoreCase("weight")){
+			System.out.println("Highest WEI: "+WEI_ll.getLast());
+
+		} else if(x.equalsIgnoreCase("acc") || x.equalsIgnoreCase("acceleration")){
+			System.out.println("Highest ACC: "+ACC_ll.getLast());
+
+		} else if(x.equalsIgnoreCase("year") || x.equalsIgnoreCase("yea")){
+			System.out.println("Highest Year: "+YEAR_ll.getLast());
+
+		} else if(x.equalsIgnoreCase("ori") || x.equalsIgnoreCase("origin") || x == "8" ){
+			System.out.println("Highest ORI: "+ORI_ll.getLast());
+
+		}
+		
 	}
+	
+	public void findAverage(String x) {
+		
+		int avg = 0;
+		int sum = 0;
 
-	public void printSorted(){
-	//Prints sorted LL
-	System.out.println("\nMPG Linked List SORTED: ");
-	System.out.println(MPG_ll);
-	
-	System.out.println("\nCYLINDERS Linked List SORTED: ");
-	System.out.println(CYL_ll);
-	
-	System.out.println("\nDISPLACEMENT Linked List SORTED: ");
-	System.out.println(DIS_ll);
-	
-	System.out.println("\nHorsePower Linked List SORTED: ");
-	System.out.println(HP_ll);
-	
-	System.out.println("\nWEIGHT Linked List SORTED: ");
-	System.out.println(WEI_ll);
-	
-	System.out.println("\nACCELERATION Linked List SORTED: ");
-	System.out.println(ACC_ll);
-	
-	System.out.println("\nYEAR Linked List SORTED: ");
-	System.out.println(YEAR_ll);
-	
-	System.out.println("\nORIGIN Linked List SORTED: ");
-	System.out.println(ORI_ll);
-	
-}
+		if( x.equalsIgnoreCase("mpg") || x.equalsIgnoreCase("1") ){
+			
+			for (int d : MPG_ll) { 
+				sum += d;
+			}
+			
+			avg = sum/MPG_ll.size();
+			
+			System.out.println("Average MPG: " + avg);
+			
+			avg = 0;
+			
+		} else if(x.equalsIgnoreCase("cyl") || x.equalsIgnoreCase("cylinders") ){
+			
+			for (int d : CYL_ll) { 
+				sum += d;
+			}
+			
+			avg = sum/CYL_ll.size();
+			
+			System.out.println("Average Cylinders: " + avg);
+			
+			avg = 0;
+		
+		} else if(x.equalsIgnoreCase("dis") || x.equalsIgnoreCase("displacement") ){
+			
+			for (int d : DIS_ll) { 
+				sum += d;
+			}
+			
+			avg = sum/DIS_ll.size();
+			
+			System.out.println("Average Displacement: " + avg);
+			
+			avg = 0;
+		
+		} else if(x.equalsIgnoreCase("hp") || x.equalsIgnoreCase("horsepower")){
+			
+			for (int d : HP_ll) { 
+				sum += d;
+			}
+			
+			avg = sum/HP_ll.size();
+			
+			System.out.println("Average HorsePower: " + avg);
+			
+			avg = 0;
+		
+		} else if(x.equalsIgnoreCase("wei") || x.equalsIgnoreCase("weight")){
+			
+			for (int d : WEI_ll) { 
+				sum += d;
+			}
+			
+			avg = sum/WEI_ll.size();
+			
+			System.out.println("Average Weight: " + avg);
+			
+			avg = 0;
 
+		} else if(x.equalsIgnoreCase("acc") || x.equalsIgnoreCase("acceleration")){
+			
+			for (int d : ACC_ll) { 
+				sum += d;
+			}
+			
+			avg = sum/ACC_ll.size();
+			
+			System.out.println("Average Acceleration: " + avg);
+			
+			avg = 0;
+
+		} else if(x.equalsIgnoreCase("year") || x.equalsIgnoreCase("yea")){
+			
+			for (int d : YEAR_ll) { 
+				sum += d;
+			}
+			
+			avg = sum/YEAR_ll.size();
+			
+			System.out.println("Average Year: " + avg);
+			
+			avg = 0;
+
+		} else if(x.equalsIgnoreCase("ori") || x.equalsIgnoreCase("origin") || x == "8" ){
+			
+			for (int d : ORI_ll) { 
+				sum += d;
+			}
+			
+			avg = sum/ORI_ll.size();
+			
+			System.out.println("Average Origin: " + avg);
+			
+			avg = 0;
+
+		}
+			
+	}
+	
+	/*
+	 * 	Sorting Methods
+	 * 
+	 */
+	
 	public void sortContents(){
 	
 	Collections.sort(MPG_ll);
@@ -396,28 +613,49 @@ public void swapValues(int indexOne, int indexTwo){
 	Collections.sort(ORI_ll);
 	Collections.sort(CAR);	
 	
-	
 }
+	 
 
+	
+	/*
+	 * 	File Control Methods
+	 * 
+	 */
+	
+	public void openAllData() {
+		try{
+			x = new Scanner(new File("AllData"));
+			
+		}catch(Exception e){
+			System.out.println("Could not find File");
+		}
+	}
+	
+	public void readAllData() {
+		
+		while(x.hasNext()) {
+			String a = x.next();
+			CAR.add(a);
+			CAR_count++;
+			}
+	}
+	
 	public void openMakeModel() {
 		
 		try{
-			x = new Scanner(new File("AllData"));
+			x = new Scanner(new File("MakeModel.txt"));
 			
 		}catch(Exception e){
 			System.out.println("Could not find File");
 		}	
 	}
 	
+	
 	public void readMakeModel() {
 		
 		while(x.hasNext()) {
-		
 		String a = x.next();
-		CAR.add(a);
-		CAR_count++;
-
-		
+		MakeModel.add(a);
 		}
 	}
 		
@@ -432,22 +670,25 @@ public void swapValues(int indexOne, int indexTwo){
 		}	
 	}
 	
+	
 	public void readFile(){
 		
-		while(x.hasNext()){						// Loop through each LINE and add variable dependent on spacing
+		while(x.hasNext()){						
 			
 			//MPG
-			String a = x.next();				// Takes first sequence and adds as string(a)
-			MPG = Integer.parseInt(a);		// Converts MPG String to double
-			MPG_ll.add(MPG);					// Adds converted MPG to LL
-			MPG_count++;						// Adds to a count for every MPG instance
-			MPG = 0;							// Resets Db back to 0
+			String a = x.next();				
+			MPG = Integer.parseInt(a);		
+			MPG_ll.add(MPG);					
+			MPG_arr.add(MPG);
+			MPG_count++;						
+			MPG = 0;
 			count++;
-
+			
 			//CYL
 			String b = x.next();
 			CYL = Integer.parseInt(b);
 			CYL_ll.add(CYL);
+			CYL_arr.add(CYL);
 			CYL_count++;
 			CYL = 0;
 			count++;
@@ -456,6 +697,7 @@ public void swapValues(int indexOne, int indexTwo){
 			String c = x.next();
 			DIS = Integer.parseInt(c);
 			DIS_ll.add(DIS);
+			DIS_arr.add(DIS);
 			DIS_count++;
 			DIS = 0;
 			count++;
@@ -464,6 +706,7 @@ public void swapValues(int indexOne, int indexTwo){
 			String d = x.next();
 			HP = Integer.parseInt(d);
 			HP_ll.add(HP);
+			HP_arr.add(HP);
 			HP_count++;
 			HP = 0;
 			count++;
@@ -472,6 +715,7 @@ public void swapValues(int indexOne, int indexTwo){
 			String e = x.next();
 			WEI = Integer.parseInt(e);
 			WEI_ll.add(WEI);
+			WEI_arr.add(WEI);
 			WEI_count++;
 			WEI = 0;
 			count++;
@@ -480,6 +724,7 @@ public void swapValues(int indexOne, int indexTwo){
 			String f = x.next();
 			ACC = Integer.parseInt(f);
 			ACC_ll.add(ACC);
+			ACC_arr.add(ACC);
 			ACC_count++;
 			ACC = 0;
 			count++;
@@ -488,6 +733,7 @@ public void swapValues(int indexOne, int indexTwo){
 			String g = x.next();
 			YEAR = Integer.parseInt(g);
 			YEAR_ll.add(YEAR);
+			YEAR_arr.add(YEAR);
 			YEAR_count++;
 			YEAR = 0;
 			count++;
@@ -496,12 +742,14 @@ public void swapValues(int indexOne, int indexTwo){
 			String h = x.next();;
 			ORI = Integer.parseInt(h);
 			ORI_ll.add(ORI);
+			ORI_arr.add(ORI);
 			ORI_count++;
 			ORI = 0;
 			count++;
 			
 		}	
 	}
+	
 	
 	public void closeFile(){
 		x.close();
